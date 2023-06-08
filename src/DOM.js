bcc6ac4ca6334acdcb8aba0cc751f8dce60ed732
sifiso <simabuz022@student.wethinkcode.co.za>
2023-06-08 15:08:56 +0200

    added display logic

--- src/DOM.js
+++ src/DOM.js
@@ -11,7 +11,37 @@ addProjectButton.addEventListener('click', () => {
 });
 
 const displayProjects = (projects) => {
+    const projectsContainer = document.querySelector("#projects");
     projects.map((project) => {
-        // loop through list and display each project.
+        const projectContainer = document.createElement("div");
+        
+        const nameContainer = document.createElement("div");
+        nameContainer.setAttribute("id", "project-name");
+        nameContainer.textContent(project.name);
+        projectContainer.appendChild(nameContainer);
+
+        const discriptionContainer = document.createElement("div");
+        discriptionContainer.setAttribute("id", "project-discription");
+        discriptionContainer.textContent(project.discription);
+        projectContainer.appendChild(discriptionContainer);
+
+        const dueDateContainer = document.createElement("div");
+        dueDateContainer.setAttribute("id", "project-dueDate");
+        dueDateContainer.textContent(project.dueDate);
+        projectContainer.appendChild(dueDateContainer);
+
+        const priorityContainer = document.createElement("div");
+        priorityContainer.setAttribute("id", "project-priority");
+        priorityContainer.textContent(project.priority);
+        projectContainer.appendChild(priorityContainer);
+
+        const editButton = document.createElement("button");
+        editButton.setAttribute("id", "edit");
+        projectContainer.appendChild(editButton);
+
+        projectsContainer.appendChild(projectContainer); 
     });
-}
+};
+
+
+export {displayProjects};
