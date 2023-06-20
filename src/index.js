879d6d8d2885b8d76c487c10a16b28d5c721bf71
sifiso <simabuz022@student.wethinkcode.co.za>
2023-06-20 14:15:02 +0200

    able to ompletly reset add task form

--- src/index.js
+++ src/index.js
@@ -10,10 +10,18 @@ const projectDiscription = document.querySelector('#project-discription');
 const dateDue = document.querySelector('#project-dueDate');
 const priority = document.querySelector('#project-priority');
 
+const resetForm = () => {
+    projectName.value = '';
+    projectDiscription.value = '';
+    dateDue.value = "";
+    priority.value = "";
+};
+
 const submitButton = document.querySelector('#submit');
 submitButton.addEventListener('click', () => {
     if(projectName.value != '' && projectDiscription.value != '' && dateDue != "" && priority.value != ''){
         saveProjects(createNewProject(projectName.value, projectDiscription.value, dateDue.value, priority.value));
         formContainer.style.display = 'none';
+        resetForm()
     };
 });
